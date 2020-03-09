39d560d208dab567f657bd87d688ecb0a3de6b60
Unknown <mitch.peake@googlemail.com>
2020-03-09 11:28:47 +0000

    Added comments
    Cleaned code and added comments to clean device.

--- Feeder/Feeder.ino
+++ Feeder/Feeder.ino
@@ -1,35 +1,31 @@
-
 #include <Servo.h>
 
+//180 closed position 
+//40 open position
+ 
 Servo myservo;  // create servo object to control a servo
-
 int pos = 0;    // variable to store the servo position
+int openPos = 20;
+int closedPos = 180;
 
 void setup() {
   Serial.begin(9600);
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-    // Initially the servo must be stopped 
-   // myservo.write(40);
-    myservo.write(180);
-    //180 closed
-    //40 open
-    //
+  myservo.write(closedPos); // Initially the servo must be moved to closed position
 }
 
 void loop() {
-  if (Serial.available() > 0) {
+  if (Serial.available() > 0) {     
     char cmd = (char) Serial.read();
     if (cmd == '1') {
-      for (pos = 180; pos >= 30; pos -= 1) { // goes from 180 degrees to 0 degrees
-        myservo.write(pos);              // tell servo to go to position in variable 'pos'
-       // delay(5);                       // waits 15ms for the servo to reach the position#
+      for (pos = closedPos; pos >= openPos; pos -= 1) { // goes from closed position (180 degrees )to open poisition (20 degrees)
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'                 
         Serial.println(pos);
       }
       delay(250);   
-        for (pos = 30; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+        for (pos = openPos; pos <= closedPos; pos += 1) { // goes from open position (20 degrees) to closed position (180 degrees)
           // in steps of 1 degree
           myservo.write(pos);              // tell servo to go to position in variable 'pos'
-         // delay(5);
           Serial.println(pos);
         }
       }
