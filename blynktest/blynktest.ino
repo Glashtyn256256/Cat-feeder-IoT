1922d77794f707b033aba3e6ea883f48397dc7c5
Recov <Recov@LAPTOP-5SU6BEGF>
2020-03-21 00:31:49 +0000

    Ben - Sending the weight data from arduino to nodeMCU using JSON.

--- blynktest/blynktest.ino
+++ blynktest/blynktest.ino
@@ -3,21 +3,22 @@
 #include <SPI.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+#include <StreamUtils.h>
 
+SoftwareSerial s(D6,D5);
 
 
 char auth[] = "KI5X0r2UxFp3zDvToscJpNlj8f1PiE2K"; //Enter the Auth codethat is given to you by blynk
 
 char ssid[] = "BrindLetting01";  //Enter your WIFI Name
-char pass[] = "";  //Enter your WIFI Password
+char pass[] = "Brind1234!";  //Enter your WIFI Password
 
-#define DOUT  3
-#define CLK  2
 
-HX711 scale;
 
 float calibration_factor = +163400;
-int test;
+int totalWeight;
 SimpleTimer timer;
 
 // This function sends Arduino's up time every second to Virtual Pin (5).
@@ -25,27 +26,77 @@ SimpleTimer timer;
 // that you define how often to send data to Blynk App.
 void sendWeightReadings()
 {
-    Blynk.virtualWrite(V7,test);  //add the value set in the app
+    Blynk.virtualWrite(V7,totalWeight);  //add the value set in the app
 }
 
 BLYNK_WRITE(V6) //Read from the value set in the blynk app from v6 pin
 {
-  test = param.asInt();
+//  test = param.asInt();
 }
 
 void setup()
 {
-  Serial.begin(115200); // See the connection status in Serial Monitor
-  Blynk.begin(auth, ssid, pass);
+   
+   s.begin(115200);
+   Serial.begin(115200); // See the connection status in Serial Monitor
+ //  pinMode(D7, INPUT);
 
-
-
+  //  pinMode(D8, OUTPUT);
+   while (!Serial) continue;
+ // Blynk.begin(auth, ssid, pass);
+ 
   //Gets called every second
-  timer.setInterval(1000L, sendWeightReadings);
+ // timer.setInterval(1000L, sendWeightReadings);
 }
+
 
 void loop()
 {
-  Blynk.run(); // Initiates Blynk
-  timer.run(); // Initiates SimpleTimer
+//int data;
+delay(1000);
+if(s.available()>0){
+ StaticJsonDocument<1000> jsonDoc;
+ //s.println(s);
+ //ReadLoggingStream loggingStream(s, Serial);
+ DeserializationError err = deserializeJson(jsonDoc, s);
+
+ //JsonObject root = jsonDoc.as<JsonObject>();
+ // auto error = deserializeJson(jsonDoc, s);
+//  Serial.println(object["weight"]);
+ //const char* ttotalWeight = jsonDoc["weight"];
+ // Serial.println(ttotalWeight);
+if (err) {
+    Serial.print(F("deserializeJson() failed with code "));
+    Serial.println(err.c_str());
+    return;
 }
+
+totalWeight = jsonDoc["weight"];
+Serial.println(totalWeight);
+}
+}
+
+//StaticJsonBuffer<1000> jsonBuffer;
+//  JsonObject& root = jsonBuffer.parseObject(s);
+//  //root.printTo(Serial);
+//  if (root == JsonObject::invalid())
+//  {
+//    Serial.println("fail");
+//    return;
+//  }
+//  Serial.println("JSON received and parsed");
+//  root.prettyPrintTo(Serial);
+//  Serial.print("Data 1 ");
+//  Serial.println("");
+//  int data1=root["data1"];
+//  Serial.print(data1);
+//  Serial.print("   Data 2 ");
+//  int data2=root["data2"];
+//  Serial.print(data2);
+//  Serial.println("");
+//  Serial.println("---------------------xxxxx--------------------");
+ 
+
+//  Blynk.run(); // Initiates Blynk
+//  timer.run(); // Initiates SimpleTimer
+  
--- weighing_scale_sketch/weighing_scale_sketch.ino
+++ weighing_scale_sketch/weighing_scale_sketch.ino
@@ -1,14 +1,24 @@
 #include "HX711.h"
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+
+SoftwareSerial s(5,6); //rx, tx
 
 #define DOUT  3
 #define CLK  2
 
 HX711 scale;
 
-float calibration_factor = +163400; //Depending on the scale may need to adjust + or -, 163400 gives us accurate result for our scale.
+float calibration_factor = +163000; //Depending on the scale may need to adjust + or -, 163400 gives us accurate result for our scale.
+//The surface you have the wieght on will effect the weight. Make sure it's on a solid surface for best readsings.
+
+float totalWeight;
+const int capacity = JSON_OBJECT_SIZE(1);
 
 void setup() {
-  Serial.begin(9600);
+//  Serial.begin(9600);
+  s.begin(115200);
+  Serial.begin(115200);
   scale.begin(DOUT, CLK);
   scale.set_scale();
   scale.tare(); //Reset the scale to 0
@@ -16,17 +26,47 @@ void setup() {
 //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
   long zero_factor = scale.read_average(); //Get a baseline reading
    scale.set_scale(calibration_factor); //Use the calibiration factor that you
+   
 }
-
+//6 has different naming conventions
+StaticJsonDocument<1000> jsonDoc;
+//JsonObject object = jsonDoc.to<JsonObject>();
 void loop() {
 
-  Serial.print("Reading: ");
+  //JsonObject root = jsonDoc.as<JsonObject>();
+ // Serial.print("Reading: ");
   if(scale.get_units() > -3 && scale.get_units() < 0){
-    Serial.print(0);
+    //Serial.print(0);
+    totalWeight = 0;
   }
   else
   {
-    Serial.print(scale.get_units() * 453.59237, 0); //Units are in lbs changing to grams
+    totalWeight = scale.get_units() * 453.59237;
+    //Serial.print(totalWeight, 0); //Units are in lbs changing to grams
   }
-  Serial.print(" grams"); 
+//  Serial.println(" grams");
+  jsonDoc["weight"].set(totalWeight);
+ // jsonDoc["raw"] = serialized("[1]");
+ // int test = (int) totalWeight;
+  //delay(1000);
+  //jsonDoc["weight"] = "help";
+ //StaticJsonBuffer<1000> jsonBuffer;
+ //JsonObject& root = jsonBuffer.createObject();
+ // root["data1"] = 100;
+  //root["data2"] = 200;
+  // root.printTo(Serial);
+//  if(s.available()>0)
+ // {
+     //test = s.read();
+   // Serial.print("hello");
+   // s.write(test);
+    //s.println("test is availiable");
+    
+    serializeJson(jsonDoc, s);
+    delay(1000);
+   // root.printTo(Serial);
+   
+  // root.printTo(Serial);
+    //root.printTo(s);
+ // }
 }
